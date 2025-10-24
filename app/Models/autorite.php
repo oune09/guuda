489968a1ecb2ae9f6ae8f96d6e08d004ed39eb3a
oune09 <deraousmane2004@gmail.com>
2025-10-24 11:57:04 +0000

    model autorite

--- app/Models/autorite.php
+++ app/Models/autorite.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class autorite extends Model
 {
-    //
+   protected $fillable = [
+    'utilisateur_id ',
+    'organisation',
+    'matricule',
+    'zone_responsabilite',
+    'statut',
+   ];
+
+   protected $casts = [
+    'statut' => 'boolean',
+   ];
+
+   public function incident()
+   {
+      return $this->hasMany(incident::class);
+   }
+
+   public function alert()
+   {
+      return $this->hasMany(alert::class);
+   }
 }
