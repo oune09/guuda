6177c5198d747bd5f73b61b24e18e4b3c7760a39
oune09 <deraousmane2004@gmail.com>
2025-10-27 15:53:13 +0000

    migration alerte

---
+++ database/migrations/2025_10_27_153024_create_alertes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alertes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('autorite_id')->constrained('autorites')->onDelete('cascade');
+            $table->foreignId('incident_id')->constrained('incidents')->ondelete('cascade')->nullble();
+            $table-enum('niveau_alerte',['info','avertissement','urgence'])->default('info');
+            $table->text('message_alerte');
+            $table->boolean('lu')->default('false');
+            $table->datetime('date_alerte');
+            $table->datetime('date_fin')->nullable();
+            $table->enum('statut_alerte',['active','terminee'])->default('active');
+            $table->string('ville');
+            $table->string('secteur');
+            $table->string('quartier');
+            $table->string('longitude');
+            $table->string('latitude');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alertes');
+    }
+};
