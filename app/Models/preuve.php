31f704d615f2a56fe77c0373fb71b2edb0fccf3b
oune09 <deraousmane2004@gmail.com>
2025-10-28 08:48:20 +0000

    model preuve

--- app/Models/preuve.php
+++ app/Models/preuve.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class preuve extends Model
 {
-    //
+    protected $fillable = [
+        'incident_id',
+        'nom_incident',
+        'type_preuve',
+        'lien_preuve',
+        'description_preuve',
+        'statut_preuve',
+    ];
+
+    protected $casts = [
+        ''
+    ];
+
+    public function  incident()
+    {
+        return $this->belongsTo(incident::class);
+    }
+
+    public function scopeStatut($query,$statut)
+    {
+        return $query->where('statut_preuve',$satut);
+    }
+    
+    public function scopeType($query,$type)
+    {
+        return $query->where('type_preuve',$type);
+    }
+
+    
+
 }
